60cf2015feeef9c047363907a6eb4f33e7a79691
Stefano.In91 <ste.ince91@gmail.com>
2022-12-13 16:03:28 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+  $paragraph = "“The dark is generous, and it is patient, and it always wins — but in the heart of its strength lies its weakness: One lone candle is enough to hold it back. Love is more than a candle. Love can ignite the stars.” — Narrator";
+  $filtered_paragraph = str_replace("Love", "***", $paragraph);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+  </head>
+  <body>
+    <h3> <?php echo $paragraph ?> </h3>
+    <p> <?php echo strlen($paragraph)?> </p>
+    <h3> <?php echo $filtered_paragraph?> </h3>
+    <p> <?php echo strlen($filtered_paragraph)?> </p>
+  </body>
+</html>
