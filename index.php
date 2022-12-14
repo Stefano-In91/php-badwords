ff87bd9e01bff90c93f2c2467dbada8c7e250aa0
Stefano.In91 <ste.ince91@gmail.com>
2022-12-14 10:20:25 +0100

    adds get form

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
   $paragraph = "“The dark is generous, and it is patient, and it always wins — but in the heart of its strength lies its weakness: One lone candle is enough to hold it back. Love is more than a candle. Love can ignite the stars.” — Narrator";
-  $filtered_paragraph = str_replace($_GET["word"], "***", $paragraph);
+  $censure = $_GET["word"];
+  $filtered_paragraph = str_replace($censure, "***", $paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +13,21 @@
     <title>BadWords</title>
   </head>
   <body>
-    <h3> <?php echo $paragraph ?> </h3>
-    <p> <?php echo strlen($paragraph)?> </p>
+    <header>
+      <form action="index.php" method="GET">
+        <label for="censure">Parola da Censurare</label>
+        <input name="word" id="censure" type="text" placeholder="Inserisci parola">
+        <button type="submit">Invia</button>
+      </form>
+    </header>
     <hr>
-    <p> La parola da filtrare è <?php echo $_GET["word"] ?> </p>
-    <h3> <?php echo $filtered_paragraph?> </h3>
-    <p> <?php echo strlen($filtered_paragraph)?> </p>
+    <main>
+      <h3> <?php echo $paragraph; ?> </h3>
+      <p>Lunghezza: <?php echo strlen($paragraph); ?> </p>
+      <hr>
+      <p> La parola da filtrare è <?php echo $censure; ?> </p>
+      <h3> <?php echo $filtered_paragraph; ?> </h3>
+      <p>Lunghezza: <?php echo strlen($filtered_paragraph); ?> </p>
+    </main>
   </body>
 </html>
